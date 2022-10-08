8298352904d6977f5aa6aa166cb19f698279cb0a
Alirio-Mieres <mie21001@byui.edu>
2022-10-08 17:32:36 -0400

    configuration service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
 
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      load: [EnvConfiguration]  
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
       }),
-      MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+      MongooseModule.forRoot( process.env.MONGODB ),
     PokemonModule,
     CommonModule,
     SeedModule
@@ -24,7 +27,6 @@ import { ConfigModule } from '@nestjs/config';
 export class AppModule {
 
   constructor() {
-    console.log(process.env)
   }
 
 }
